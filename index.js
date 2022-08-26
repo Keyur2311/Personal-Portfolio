e858f62928ee3178292b74915ce5fdbcf7504248
Keyur Patel <patelkeyur2311@gmail.com>
2022-08-27 01:58:50 +0530

    added

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// document.getElementById('home').onclick = function () { myfunc() };
+// document.getElementById('about').onclick = function () { myfunc() };
+// document.getElementById('skill').onclick = function () { myfunc() };
+// document.getElementById('project').onclick = function () { myfunc() };
+// document.getElementById('contact').onclick = function () { myfunc() };
+
+// function myfunc() {
+
+// }
